dea85ea8b6ce17da65c22450269a51055965255f
Aimeos <aimeos@aimeos.org>
2017-05-02 13:37:45 +0200

    Fix for PHP 5.4/5.5 compatibility

--- lib/custom/tests/MW/Logger/FlowTest.php
+++ lib/custom/tests/MW/Logger/FlowTest.php
@@ -75,7 +75,7 @@ class FlowTest extends \PHPUnit\Framework\TestCase
 
 	public function testBadPriority()
 	{
-		$this->expectException( '\\Aimeos\\MW\\Logger\\Exception' );
+		$this->setExpectedException( '\\Aimeos\\MW\\Logger\\Exception' );
 		$this->object->log( 'error', -1 );
 	}
 

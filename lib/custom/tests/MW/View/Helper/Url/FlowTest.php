983aaf0078f6dfc90bbfcb67685234a7c397df2a
Aimeos <aimeos@aimeos.org>
2018-06-26 12:21:31 +0200

    Simplified environment setup

--- lib/custom/tests/MW/View/Helper/Url/FlowTest.php
+++ lib/custom/tests/MW/View/Helper/Url/FlowTest.php
@@ -9,21 +9,12 @@
 namespace Aimeos\MW\View\Helper\Url;
 
 
-/**
- * Test class for \Aimeos\MW\View\Helper\Url\Flow.
- */
 class FlowTest extends \PHPUnit\Framework\TestCase
 {
 	private $object;
-	private $mockRouter;
+	private $buildMock;
 
 
-	/**
-	 * Sets up the fixture, for example, opens a network connection.
-	 * This method is called before a test is executed.
-	 *
-	 * @access protected
-	 */
 	protected function setUp()
 	{
 		if( !class_exists( '\\Neos\\Flow\\Mvc\\Routing\\UriBuilder' ) ) {
@@ -32,51 +23,15 @@ class FlowTest extends \PHPUnit\Framework\TestCase
 
 		$view = new \Aimeos\MW\View\Standard();
 
-		$mockHttpRequest = $this->getMockBuilder( 'Neos\Flow\Http\Request' )
-			->setMethods( array( 'getBaseUri' ) )
-			->disableOriginalConstructor()
-			->getMock();
-		$mockHttpRequest->expects( $this->any() )
-			->method( 'getBaseUri' )
-			->will( $this->returnValue( 'http://localhost/' ) );
-
-		$mockMainRequest = $this->getMockBuilder( 'Neos\Flow\Mvc\ActionRequest' )
-			->setMethods( array( 'getControllerObjectName', 'getArgumentNamespace' ) )
-			->setConstructorArgs( array( $mockHttpRequest ) )
-			->getMock();
-		$mockMainRequest->expects( $this->any() )
-			->method( 'getArgumentNamespace' )
-			->will( $this->returnValue( 'ai' ) );
-
-		$this->mockRouter = $this->getMockBuilder( 'Neos\Flow\Mvc\Routing\Router' )->getMock();
-		$mockEnv = $this->getMockBuilder( 'Neos\Flow\Utility\Environment' )
-			->setMethods( array( 'isRewriteEnabled' ) )
+		$this->buildMock = $this->getMockbuilder( '\\Neos\\Flow\\Mvc\\Routing\\UriBuilder' )
+			->setMethods( array( 'uriFor' ) )
 			->disableOriginalConstructor()
 			->getMock();
 
-		$builder = new \Neos\Flow\Mvc\Routing\UriBuilder();
-		$builder->setRequest( $mockMainRequest );
-
-		$objectReflection = new \ReflectionObject( $builder );
-
-		$property = $objectReflection->getProperty( 'router' );
-		$property->setAccessible( true );
-		$property->setValue( $builder, $this->mockRouter );
-
-		$property = $objectReflection->getProperty( 'environment' );
-		$property->setAccessible( true );
-		$property->setValue( $builder, $mockEnv );
-
-		$this->object = new \Aimeos\MW\View\Helper\Url\Flow( $view, $builder, array( 'site' => 'unittest' ) );
+		$this->object = new \Aimeos\MW\View\Helper\Url\Flow( $view, $this->buildMock, array( 'site' => 'unittest' ) );
 	}
 
 
-	/**
-	 * Tears down the fixture, for example, closes a network connection.
-	 * This method is called after a test is executed.
-	 *
-	 * @access protected
-	 */
 	protected function tearDown()
 	{
 		$this->object = null;
@@ -85,60 +40,60 @@ class FlowTest extends \PHPUnit\Framework\TestCase
 
 	public function testTransform()
 	{
-		$this->mockRouter->expects( $this->once() )->method( 'resolve' )
-			->will( $this->returnValue( 'shop/catalog/lists') );
+		$this->buildMock->expects( $this->once() )->method( 'uriFor' )
+			->will( $this->returnValue( '/shop/catalog/lists') );
 
-		$this->assertEquals( '/index.php/shop/catalog/lists', $this->object->transform( 'shop', 'catalog', 'lists' ) );
+		$this->assertEquals( '/shop/catalog/lists', $this->object->transform( 'shop', 'catalog', 'lists' ) );
 	}
 
 
 	public function testTransformSlashes()
 	{
-		$this->mockRouter->expects( $this->once() )->method( 'resolve' )
-			->will( $this->returnValue( 'shop/catalog/lists?test=a%2Fb') );
+		$this->buildMock->expects( $this->once() )->method( 'uriFor' )
+			->will( $this->returnValue( '/shop/catalog/lists?test=a%2Fb') );
 
-		$this->assertEquals( '/index.php/shop/catalog/lists?test=a%2Fb', $this->object->transform( 'shop', 'catalog', 'lists', array( 'test' => 'a/b' ) ) );
+		$this->assertEquals( '/shop/catalog/lists?test=a%2Fb', $this->object->transform( 'shop', 'catalog', 'lists', array( 'test' => 'a/b' ) ) );
 	}
 
 
 	public function testTransformArrays()
 	{
-		$this->mockRouter->expects( $this->once() )->method( 'resolve' )
-			->will( $this->returnValue( 'shop/catalog/list?test%5B0%5D=a&test%5B1%5D=b') );
+		$this->buildMock->expects( $this->once() )->method( 'uriFor' )
+			->will( $this->returnValue( '/shop/catalog/list?test%5B0%5D=a&test%5B1%5D=b') );
 
-		$this->assertEquals( '/index.php/shop/catalog/list?test%5B0%5D=a&test%5B1%5D=b', $this->object->transform( 'shop', 'catalog', 'lists', array( 'test' => array( 'a', 'b' ) ) ) );
+		$this->assertEquals( '/shop/catalog/list?test%5B0%5D=a&test%5B1%5D=b', $this->object->transform( 'shop', 'catalog', 'lists', array( 'test' => array( 'a', 'b' ) ) ) );
 	}
 
 
 	public function testTransformTrailing()
 	{
-		$this->mockRouter->expects( $this->once() )->method( 'resolve' )
-			->will( $this->returnValue( 'shop/catalog/lists') );
+		$this->buildMock->expects( $this->once() )->method( 'uriFor' )
+			->will( $this->returnValue( '/shop/catalog/lists#a/b') );
 
-		$this->assertEquals( '/index.php/shop/catalog/lists#a/b', $this->object->transform( 'shop', 'catalog', 'lists', [], array( 'a', 'b' ) ) );
+		$this->assertEquals( '/shop/catalog/lists#a/b', $this->object->transform( 'shop', 'catalog', 'lists', [], array( 'a', 'b' ) ) );
 	}
 
 
 	public function testTransformAbsolute()
 	{
-		$this->mockRouter->expects( $this->once() )->method( 'resolve' )
-			->will( $this->returnValue( 'shop/catalog/lists') );
+		$this->buildMock->expects( $this->once() )->method( 'uriFor' )
+			->will( $this->returnValue( 'http://localhost/shop/catalog/lists') );
 
 		$options = array( 'absoluteUri' => true );
 		$result = $this->object->transform( 'shop', 'catalog', 'lists', [], [], $options );
 
-		$this->assertEquals( 'http://localhost/index.php/shop/catalog/lists', $result );
+		$this->assertEquals( 'http://localhost/shop/catalog/lists', $result );
 	}
 
 
 	public function testTransformConfig()
 	{
-		$this->mockRouter->expects( $this->once() )->method( 'resolve' )
-			->will( $this->returnValue( 'shop/catalog/lists') );
+		$this->buildMock->expects( $this->once() )->method( 'uriFor' )
+			->will( $this->returnValue( '/shop/catalog/lists') );
 
 		$options = array( 'package' => 'test', 'subpackage' => 'subtest', 'format' => 'fmt' );
 		$result = $this->object->transform( 'shop', 'catalog', 'lists', [], [], $options );
 
-		$this->assertEquals( '/index.php/shop/catalog/lists', $result );
+		$this->assertEquals( '/shop/catalog/lists', $result );
 	}
 }
